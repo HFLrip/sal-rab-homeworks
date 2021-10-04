2bf677399d3ed2e3961a5489cf0fe5360ef69d2e
HFLrip <hflrip@mail.ru>
2021-10-04 21:36:14 +0300

    1 task done full

--- task_1/solution.js
+++ task_1/solution.js
@@ -27,6 +27,8 @@ function addInCartCountChange(value) {
 
     // Задание №1.2. Увеличение количества в корзине
     let oldValue = value;
+    let newValue = oldValue + 1;
+
 
     // создайте переменную newValue
     // присвойте ей значение oldValue
@@ -40,6 +42,9 @@ function addInCartCountChange(value) {
 function addInCartSumChange(sum, delta) {
     let oldSum = sum;
     let difference = delta;
+    let newSum = oldSum + difference;
+    let newSumText = newSum + ' ₽';
+    newSumText = `${newSum} ₽`;
 
     // Задание №1.3. Увеличение суммы в корзине
 
